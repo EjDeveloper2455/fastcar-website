2627b73cca875ff9b5281b3961d42cfb7cb56d1e
EjDeveloper2455 <133950205+EjDeveloper2455@users.noreply.github.com>
2023-06-01 20:02:48 -0600

    Actualizacion server 1.0

---
+++ src/App2.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Pagina 2</h1>
+      <img src="https://media.revistagq.com/photos/5ca5e602944b834654eb8fe6/master/pass/los_10_coches_mas_caros_del_mundo_131086603.jpg" alt="" />
+    </>
+  )
+}
+
+export default App
